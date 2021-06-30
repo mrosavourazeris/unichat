1b2e48a4c12bcc28c2fccc5477b955e2cebb6b06
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-06-29 20:58:43 -0500

    Added more code to Chats.js component. Logout button and ChatEngine.

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { ChatEngine } from 'react-chat-engine'
+import { auth } from '../firebase'
 
 const Chats = () => {
+
+  const history = useHistory()
+
+  const handleLogout = async() => {
+    await auth.signOut()
+    history.push('/')
+  }
+
   return (
-    <h1>Chats</h1>
+    <div className="chats-page">
+        <div className="nav-bar">
+          <div className="logo-tab">
+            Unichat
+          </div>
+          <div onClick={handleLogout} className="logout-tab">
+            Logout
+          </div>
+        </div>
+
+        <ChatEngine
+          height='calc(100vh - 66px)'
+          projectId='074f403a-9da1-4989-9a07-c8931ca276ab'
+          userName='.'
+          userSecret='.'
+        />
+
+    </div>
   )
 }
 
