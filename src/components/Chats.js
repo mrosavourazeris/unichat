3161a3bb3556c18901e91d81bb04c22f21841b94
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-07-01 20:43:57 -0500

    adding useEffect to Chat.js component so we can show the chat for the specific user that is logged in.

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,9 +1,10 @@
-import React, {useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { ChatEngine } from 'react-chat-engine'
 import { auth } from '../firebase'
 
 import { useAuth } from '../contexts/AuthContext'
+import axios from 'axios'
 
 const Chats = () => {
 
@@ -18,6 +19,22 @@ const Chats = () => {
     history.push('/')
   }
 
+  useEffect (() => {
+    if(!user) {
+      history.push('/')
+
+      return 
+    }
+
+    axios.get('https://api.chatengine.io/users/me',{
+      headers: {
+        'projet-id': '074f403a-9da1-4989-9a07-c8931ca276ab',
+        'user-name': user.email,
+        'user-secret': user.uid,
+      }
+    })
+
+  }, [user,history])
 
 
   return (
