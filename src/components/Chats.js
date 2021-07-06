475d4eb89fe1675582f2a89a1a0bb0b854048a1a
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-07-05 21:12:04 -0500

    Fixed issue with chat not loading. Was an issue with the spelling of project.

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, {  useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { ChatEngine } from 'react-chat-engine'
 import { auth } from '../firebase'
@@ -10,6 +10,7 @@ const Chats = () => {
 
   const history = useHistory()
   const { user } = useAuth();
+  const [loading, setLoading] = useState(true)
 
   console.log(user)
 
@@ -19,22 +20,53 @@ const Chats = () => {
     history.push('/')
   }
 
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], 'userPhoto.jpg', { type: 'image/jpeg' })
+
+  }
+
   useEffect (() => {
     if(!user) {
       history.push('/')
 
-      return 
+      return; 
     }
 
     axios.get('https://api.chatengine.io/users/me',{
       headers: {
-        'projet-id': '074f403a-9da1-4989-9a07-c8931ca276ab',
+        'project-id': '074f403a-9da1-4989-9a07-c8931ca276ab',
         'user-name': user.email,
         'user-secret': user.uid,
       }
     })
+    .then(() => {
+      setLoading(false)
+    })
+    .catch(() => {
+      let formdata = new FormData()
+      formdata.append('email', user.email);
+      formdata.append('username', user.email);
+      formdata.append('secret', user.uid);
+
+      getFile(user.photoURL)
+        .then((avatar) => {
+          formdata.append('avatar', avatar, avatar.name)
+
+          axios.post('https://api.chatengine.io/users/',
+            formdata,
+            { headers: { 'private-key': 'c9cc3dca-b791-4fed-a216-cbcef4e0be85'}}
+          )
+          .then(() => setLoading(false))
+          .catch((error) => console.log(error))
+        })
+    })
 
   }, [user,history])
+
+  if(!user || loading) return 'Loading...'
 
 
   return (
@@ -50,9 +82,9 @@ const Chats = () => {
 
         <ChatEngine
           height='calc(100vh - 66px)'
-          projectId='074f403a-9da1-4989-9a07-c8931ca276ab'
-          userName='.'
-          userSecret='.'
+          projectID='074f403a-9da1-4989-9a07-c8931ca276ab'
+          userName={user.email}
+          userSecret={user.uid}
         />
 
     </div>
